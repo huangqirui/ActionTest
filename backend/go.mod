5992be14d434385f4212d94095b15234306b18e7
huangqirui <pauldhuang@hotmail.com>
2022-02-04 01:10:38 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .DS_Store
    	new file:   backend/.DS_Store
    	new file:   backend/go.mod
    	new file:   backend/hello/.DS_Store
    	new file:   backend/hello/hello_test.go
    	new file:   backend/hello/hellog.go
    	new file:   backend/main.go

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/huangqirui/ActionTest/backend
+
+go 1.17
---
+++ backend/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/huangqirui/ActionTest/backend/hello"
+)
+
+func main() {
+	fmt.Println(hello.Greet())
+}
